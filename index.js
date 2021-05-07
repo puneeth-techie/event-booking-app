4106d9b08bfd635ac09f8cd28ba8973931759405
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-05-07 17:29:05 +0530

    Initial commit.

---
+++ index.js
@@ -0,0 +1,15 @@
+import { ApolloServer } from "apollo-server";
+import { resolvers, typeDefs } from "./server/graphql";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(() => {
+  console.log(`
+        Server is up and running!
+        Listening on port 4000
+        Query at https://studio.apollographql.com/dev
+    `);
+});
