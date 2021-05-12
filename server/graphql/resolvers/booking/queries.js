4a2521f5c33e5f7b562b8429902d7b62af654de4
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-05-12 17:45:44 +0530

    Implemented get auth user event booking lists.

--- server/graphql/resolvers/booking/queries.js
+++ server/graphql/resolvers/booking/queries.js
@@ -1,12 +1,18 @@
 import { bookingModel } from "../../../models/index.js";
+import { authUser } from "../../../utils/index.js";
 
 export const Queries = {
-  getMyBookings: async () => {
+  getMyBookings: async (_, __, { req }) => {
     try {
-      const bookings = await bookingModel.find();
-      return bookings;
+      const verifiedUser = await authUser({ req });
+      if (!verifiedUser) {
+        throw new Error("Authentication required. Please login");
+      } else {
+        const bookings = await bookingModel.find({ user: verifiedUser._id });
+        return bookings;
+      }
     } catch (error) {
-      console.log(`GET_ALL_BOOKINGS: ${error.message}`);
+      console.log(`GET_MY_BOOKINGS: ${error.message}`);
       throw error;
     }
   },
